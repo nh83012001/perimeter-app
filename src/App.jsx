43407454288f7f1bde80027832342f2a8b322176
Nick Hall <nh83012001@gmail.com>
2024-07-30 15:01:52 -0700

    create-vite-app and add mapbox

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import './App.css';
+import { useEffect, useRef, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+import MapboxDraw from '@mapbox/mapbox-gl-draw';
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
+
+const paragraphStyle = {
+  fontFamily: 'Open Sans',
+  margin: 0,
+  fontSize: 13,
+};
+
+const App = () => {
+  const mapContainerRef = useRef();
+  const mapRef = useRef();
+  const [roundedArea, setRoundedArea] = useState();
+
+  useEffect(() => {
+    mapboxgl.accessToken =
+      'pk.eyJ1Ijoibmlja2hhbGwxMjIiLCJhIjoiY2xhampqYW55MGQ0bDNvczludDY4NzNqNyJ9.tR7LZPyyaJwG-6yc2IOdow';
+    // mapboxgl.accessToken = import.meta.process.env.VITE_MAPBOX_TOKEN;
+
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/satellite-v9',
+      center: [-91.874, 42.76],
+      zoom: 12,
+    });
+
+    const draw = new MapboxDraw({
+      displayControlsDefault: false,
+      controls: {
+        polygon: true,
+        trash: true,
+      },
+      defaultMode: 'draw_polygon',
+    });
+    mapRef.current.addControl(draw);
+
+    mapRef.current.on('draw.create', updateArea);
+    mapRef.current.on('draw.delete', updateArea);
+    mapRef.current.on('draw.update', updateArea);
+
+    function updateArea(e) {
+      const data = draw.getAll();
+      if (data.features.length > 0) {
+        // when you have completed drawing a polygon
+      } else {
+        setRoundedArea();
+        if (e.type !== 'draw.delete') alert('Click the map to draw a polygon.');
+      }
+    }
+  }, []);
+
+  return (
+    <>
+      <div ref={mapContainerRef} id="map" style={{ height: '100%' }}></div>
+      <div
+        className="calculation-box"
+        style={{
+          height: 75,
+          width: 150,
+          position: 'absolute',
+          bottom: 40,
+          left: 10,
+          backgroundColor: 'rgba(255, 255, 255, 0.9)',
+          padding: 15,
+          textAlign: 'center',
+        }}
+      >
+        <p style={paragraphStyle}>Click the map to draw a polygon.</p>
+        <div id="calculated-area">
+          {roundedArea && (
+            <>
+              <p style={paragraphStyle}>
+                <strong>{roundedArea}</strong>
+              </p>
+              <p style={paragraphStyle}>square meters</p>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
